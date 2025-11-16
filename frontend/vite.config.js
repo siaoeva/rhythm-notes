f23870f7a0468a6b47aa90566790e7ce2c0161fc
eva <85379882+siaoeva@users.noreply.github.com>
2025-11-16 08:15:52 -0500

    integrated login frontend and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // forward any request starting with /auth to Flask backend
+      '/auth': 'http://localhost:5000',
+    },
+  },
 })
